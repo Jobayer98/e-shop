0fb881ba65510a0cb6b309e5e5f54d98544f6a25
Jobayer98 <joobayerrahman0220@gmail.com>
2023-12-28 10:23:39 +0600

    resturce folder

--- src/index.js
+++
@@ -1,7 +0,0 @@
-require('./db/db');
-const { port } = require('./config/config');
-const app = require('./app');
-
-app.listen(port, () => {
-  console.log(`Server is listening on  http://localhost:${port}`);
-});
