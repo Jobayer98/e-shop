6dcecf34a5ed145307394564c66d0b802d77d030
Jobayer98 <joobayerrahman0220@gmail.com>
2023-12-28 10:20:41 +0600

    add category and product model

---
+++ server/src/models/productModel.js
@@ -0,0 +1,141 @@
+const mongoose = require('mongoose');
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+categorySchema.set('toObject', {
+  virtuals: true,
+});
+categorySchema.set('toJSON', {
+  virtuals: true,
+});
+
+categorySchema.virtual('products', {
+  ref: 'Product',
+  localField: '_id',
+  foreignField: 'category',
+});
+
+exports.Category = mongoose.model('Category', categorySchema);
+
+const subCategorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+});
+
+subCategorySchema.set('toObject', {
+  virtuals: true,
+});
+subCategorySchema.set('toJSON', {
+  virtuals: true,
+});
+
+subCategorySchema.virtual('products', {
+  ref: 'Product',
+  localField: '_id',
+  foreignField: 'subCategory',
+});
+
+exports.SubCategory = mongoose.model('SubCategory', subCategorySchema);
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    variants: [
+      {
+        color: {
+          type: String,
+          required: true,
+        },
+        sizes: [
+          {
+            size: {
+              type: String,
+              required: true,
+            },
+            available: {
+              type: Boolean,
+              default: true,
+              required: true,
+            },
+            quantity: {
+              type: Number,
+              required: true,
+            },
+          },
+        ],
+      },
+    ],
+    image_url: {
+      type: String,
+      // required: true,
+    },
+    cover_images_url: [
+      {
+        type: String,
+      },
+    ],
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      default: 0,
+    },
+    reviews: [
+      {
+        rating: Number,
+        comment: String,
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User',
+          required: true,
+        },
+      },
+    ],
+    inStock: {
+      type: Boolean,
+      default: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Category',
+      required: true,
+    },
+    subCategory: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'SubCategory',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+exports.Product = mongoose.model('Product', productSchema);
